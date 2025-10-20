ffd46f13bd10d68990721c9f4e5116e00c2c7942
marcolourenco1976 <marcolourenco1976@gmail.com>
2025-10-20 12:45:09 +0000

    31

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCk-L6r_p9SPxx_WmuPnzEMcEAlDfLlbJk",
   authDomain: "gestao-maconica.firebaseapp.com",
   projectId: "gestao-maconica",
-  storageBucket: "gestao-maconica.appspot.com",
+  storageBucket: "gestao-maconica.firebasestorage.app",
   messagingSenderId: "57851106083",
   appId: "1:57851106083:web:0572445f0f856d22347296",
   measurementId: "G-F1ZZ7XB7DQ"

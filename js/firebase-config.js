8fef7c020785791a47e2d9dc7daa60f3deafc20d
marcolourenco1976 <marcolourenco1976@gmail.com>
2025-10-20 13:01:22 +0000

    39

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,11 @@
 
-// Substitua estas configurações pelas do seu projeto Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "gestao-maconica",
+  "appId": "1:57851106083:web:0572445f0f856d22347296",
+  "storageBucket": "gestao-maconica.appspot.com",
+  "apiKey": "AIzaSyCk-L6r_p9SPxx_WmuPnzEMcEAlDfLlbJk",
+  "authDomain": "gestao-maconica.firebaseapp.com",
+  "messagingSenderId": "57851106083"
 };
 
 // Inicializa o Firebase

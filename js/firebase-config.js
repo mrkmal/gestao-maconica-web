b4d6fd10dbedf917c4ff0c82259e34913d5c0b06
marcolourenco1976 <marcolourenco1976@gmail.com>
2025-10-20 12:43:25 +0000

    30

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCk-L6r_p9SPxx_WmuPnzEMcEAlDfLlbJk",
+  authDomain: "gestao-maconica.firebaseapp.com",
+  projectId: "gestao-maconica",
+  storageBucket: "gestao-maconica.appspot.com",
+  messagingSenderId: "57851106083",
+  appId: "1:57851106083:web:0572445f0f856d22347296",
+  measurementId: "G-F1ZZ7XB7DQ"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
